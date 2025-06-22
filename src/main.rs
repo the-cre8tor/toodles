3f79091c535977f9919f45aa70122a11bb112fc9
Alexander Nitiola <anitiola@aellacredit.com>
2025-06-22 04:49:21 +0100

    created the runner function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::Result;
+use ratatui::{
+    DefaultTerminal,
+    crossterm::event::{self, Event, KeyCode},
+};
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let terminal = ratatui::init();
+    let result = run(terminal);
+
+    ratatui::restore();
+
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        // Rendering
+
+        // Input handling
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Esc => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
 }
